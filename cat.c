d17d607520f5359bdc9f16a5e0dd3341a0acd88a
vahidsoftic <148148593+vahidsoftic@users.noreply.github.com>
2025-05-09 05:10:19 +0200

    Add files via upload

--- cat.c
+++ cat.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <cs50.h>
+
+void meow(int n);
+
 
 int main(void)
 {
-    int i = 3;
-    while (i > 0){
-        printf("meow\n");
-        i--;
+    int n;
+    do {
+         n = get_int("Number of meows? ");
     }
+    while(n < 1);
+    meow(n);
+}
+void meow(int n) {
+    for (int i = 0; i < n; i++)
+  {  printf("meow\n");}
 }
