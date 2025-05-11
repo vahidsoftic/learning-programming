93d79fd7643f39eb09ecd35f2a2fa4c8fb1cbadb
vahidsoftic <148148593+vahidsoftic@users.noreply.github.com>
2025-05-11 03:39:48 +0200

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    for (int n =0; n < 3; n++){
+        for (int i = 0; i < 3; i++)
+        {
+            printf("#");
+            }
+            printf("\n");
+        }
+}
